b72d091ad8184bc44f42804a4e3fe637bdc5afea
Michel Bretschneider <embix@gmx.net>
2015-07-25 15:37:52 +0200

    add about page

---
+++ ContosoUniversity/ViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public Nullable<DateTime> EnrollmentDate { get; set; }
+
+        public Int32 StudentCount { get; set; }
+    }
+}
