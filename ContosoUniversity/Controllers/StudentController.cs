08bd5cb8942bdd4c4f63c6895c093b71cd8120d2
Michel Bretschneider <embix@gmx.net>
2015-07-25 12:18:26 +0200

    fix typo

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -86,7 +86,7 @@ namespace ContosoUniversity.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID, LastName, FirstMidName, EnrollmentDate")]Student student)
+        public ActionResult Edit([Bind(Include = "Id, LastName, FirstMidName, EnrollmentDate")]Student student)
         {
             try
             {
