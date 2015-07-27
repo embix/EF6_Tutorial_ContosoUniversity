f9489500f1a934ab565499a663f7917edb5027e2
Michel Bretschneider <embix@gmx.net>
2015-07-27 21:56:04 +0200

    render as date

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
@@ -8,6 +9,9 @@ namespace ContosoUniversity.Models
         public Int32 Id { get; set; }
         public String LastName { get; set; }
         public String FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
