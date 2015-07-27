faf96cb8877fcceeb32c0fb55dc234b8be519441
Michel Bretschneider <embix@gmx.net>
2015-07-27 22:02:02 +0200

    add stupid (as in: too short) length restrictions

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -7,7 +7,11 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public Int32 Id { get; set; }
+
+        [StringLength(50)]
         public String LastName { get; set; }
+
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public String FirstMidName { get; set; }
 
         [DataType(DataType.Date)]
