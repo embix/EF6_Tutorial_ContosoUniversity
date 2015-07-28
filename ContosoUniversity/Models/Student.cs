ed32fa674ea6ee1fc2ae658bddaff4cbf43b55be
Michel Bretschneider <embix@gmx.net>
2015-07-28 14:15:49 +0200

    mark fields as required

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -9,16 +9,27 @@ namespace ContosoUniversity.Models
     {
         public Int32 Id { get; set; }
 
-        [StringLength(50)]
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
+        [Display(Name = "Last Name")]
         public String LastName { get; set; }
 
+        [Required]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         [Column("FirstName")]
+        [Display(Name = "First Name")]
         public String FirstMidName { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public String FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
